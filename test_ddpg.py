8b0e6b9ae74c935fa716627df0b8ebbb6f93ad00
mateusznajdek <mateusz14321@gmail.com>
2020-05-22 00:47:41 +0200

    Added callbacks metrics for tensorboard for DDPG

--- test_ddpg.py
+++ test_ddpg.py
@@ -5,33 +5,64 @@ import numpy as np
 
 import object_data
 from envs.panda_grasp_env import PandaGraspGymEnv
-from stable_baselines.ddpg.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import DDPG
-from stable_baselines.ddpg.noise import OrnsteinUhlenbeckActionNoise
+from stable_baselines.common.evaluation import evaluate_policy
+from stable_baselines.her import HERGoalEnvWrapper
+import argparse
 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(os.path.dirname(current_dir))
 os.sys.path.insert(0, parent_dir)
 
 
-def main():
-    panda_env = PandaGraspGymEnv(urdf_root=object_data.getDataPath(), is_rendering=True, use_ik=True, is_discrete=False,
-                                 num_controlled_joints=7)
+def main(_model_name, _trained_models_dir, _should_eval, _eval_num_episodes, _should_render):
 
-    env = DummyVecEnv([lambda: panda_env])
-    param_noise = None
-    n_actions = panda_env.action_space.shape[-1]
-    action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
-    model = DDPG(MlpPolicy, panda_env, verbose=1, param_noise=param_noise, action_noise=action_noise,
-                 tensorboard_log="tensorboard/")
-    model.load("tmp/best_model.pkl")
-    obs = env.reset()
-    for i in range(10000):
-        action, _states = model.predict(obs)
-        obs, rewards, done, info = env.step(action)
-        env.render(mode='human')
+    panda_env = PandaGraspGymEnv(urdf_root=object_data.getDataPath(),
+                                 is_rendering=(not _should_eval) or _should_render,
+                                 use_ik=True,
+                                 is_discrete=False,
+                                 num_controlled_joints=7,
+                                 reward_type="dense",
+                                 draw_workspace=True)
+
+    model = DDPG.load(_trained_models_dir + "/" + _model_name)
+
+    env = HERGoalEnvWrapper(panda_env)
+
+    if _should_eval:
+        episode_rewards, episode_lengths, episode_success = evaluate_policy(model, env,
+                                                                            n_eval_episodes=_eval_num_episodes,
+                                                                            render=(not _should_eval) or _should_render,
+                                                                            deterministic=True,
+                                                                            return_episode_rewards=True)
+        print("Final Reward DDPG: {}, Episodes: {}, Episode Length: {}, Success Rate: {}"
+              .format(
+                np.mean(episode_rewards),
+                _eval_num_episodes,
+                np.mean(episode_lengths),
+                np.mean(episode_success)))
+
+    else:
+        obs = env.reset()
+        for i in range(2000):
+            action, _states = model.predict(obs)
+            obs, rewards, done, info = env.step(action)
+            env.render(mode='human')
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model', help='Name for a model to test',
+                        default='best_model.zip', type=str, required=False)
+    parser.add_argument('-d', '--dir', help='Directory in which model is located',
+                        default='trainedModels', type=str, required=False)
+    parser.add_argument('-e', '--eval', help='Should perform evaluation on model [True/False]', required=False,
+                        default=True, type=lambda x: ((str(x).lower() == 'true') or (str(x).lower() == 'True')))
+    parser.add_argument('-n', '--num', help='Number of episodes for evaluation (if eval set to True)',
+                        default=100, type=int, required=False)
+    parser.add_argument('-r', '--render', help='Should render images in human mode, only if eval=True', required=False,
+                        default=True, type=lambda x: ((str(x).lower() == 'true') or (str(x).lower() == 'True')))
+    args = parser.parse_args()
+
+    main(args.model, args.dir, args.eval, args.num, args.render)
--- train_ddpg.py
+++ train_ddpg.py
@@ -2,61 +2,77 @@ from stable_baselines.results_plotter import load_results, ts2xy
 
 import object_data
 from envs import PandaGraspGymEnv
-from stable_baselines.ddpg.policies import MlpPolicy
+from stable_baselines.ddpg.policies import MlpPolicy, LnMlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.ddpg.noise import OrnsteinUhlenbeckActionNoise
 from stable_baselines import DDPG
 import numpy as np
 import os
-from stable_baselines.bench import Monitor
-from stable_baselines import results_plotter
-import matplotlib.pyplot as plt
+from CustomMonitor import CustomMonitor
+from stable_baselines.common.callbacks import CheckpointCallback, EvalCallback
+from custom_callbacks import MeanHundredEpsTensorboardCallback, SuccessRateTensorboardCallback
+from stable_baselines.her import HERGoalEnvWrapper
 
 best_mean_reward, n_steps = -np.inf, 0
-log_dir = "tmp/"
+log_dir = "log/"
+eval_log_dir = "log/eval/"
+trained_models_dir = "trainedModels/"
 
 
-def callback(_locals, _globals):
-    """
-    Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
-    :param _locals: (dict)
-    :param _globals: (dict)
-    """
-    global n_steps, best_mean_reward
-    # Print stats every 1000 calls
-    if (n_steps + 1) % 1000 == 0:
-        # Evaluate policy training performance
-        x, y = ts2xy(load_results(log_dir), 'timesteps')
-        if len(x) > 0:
-            mean_reward = np.mean(y[-100:])
-            print(x[-1], 'timesteps')
-            print(
-                "Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
-
-            # New best model, you could save the agent here
-            if mean_reward > best_mean_reward:
-                best_mean_reward = mean_reward
-                # Example for saving best model
-                print("Saving new best model")
-                _locals['self'].save(log_dir + 'best_model.pkl')
-    n_steps += 1
-    return True
+def get_environment():
+    env = PandaGraspGymEnv(urdf_root=object_data.getDataPath(),
+                           is_rendering=False,
+                           use_ik=True,
+                           is_discrete=False,
+                           num_controlled_joints=7,
+                           lock_rotation=True,
+                           max_step_count=1000,
+                           reward_type='dense')
+    return env
 
 
 def main():
     os.makedirs(log_dir, exist_ok=True)
-    panda_env = PandaGraspGymEnv(urdf_root=object_data.getDataPath(), is_rendering=False, use_ik=True, is_discrete=False,
-                                 num_controlled_joints=7)
-    panda_env = Monitor(panda_env, log_dir, allow_early_resets=True)
+    os.makedirs(eval_log_dir, exist_ok=True)
+    os.makedirs(trained_models_dir, exist_ok=True)
 
-    param_noise = None
+    panda_env = HERGoalEnvWrapper(CustomMonitor(get_environment(), log_dir))
+    eval_env = HERGoalEnvWrapper(CustomMonitor(get_environment(), eval_log_dir))
+
+    every_n_steps_callback = CheckpointCallback(50000, trained_models_dir)
+    mean_hundred_eps_callback = MeanHundredEpsTensorboardCallback(log_dir)
+    succ_rate_callback = SuccessRateTensorboardCallback(log_dir)
+    eval_callback = EvalCallback(eval_env,
+                                 best_model_save_path=trained_models_dir,
+                                 log_path=log_dir,
+                                 eval_freq=100000,
+                                 deterministic=True,
+                                 render=False,
+                                 n_eval_episodes=10)
+
+    time_steps = 1000000
+
     n_actions = panda_env.action_space.shape[-1]
     action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
-    model = DDPG(MlpPolicy, panda_env, verbose=1, param_noise=param_noise, action_noise=action_noise, tensorboard_log="tensorboard/",gamma=0.9)
-    time_steps = 1000000
-    model.learn(total_timesteps=time_steps, callback=callback)
-    results_plotter.plot_results([log_dir], time_steps, results_plotter.X_TIMESTEPS, "DDPG LunarLander")
-    plt.show()
+
+    model = DDPG(MlpPolicy,
+                 panda_env,
+                 verbose=1,
+                 param_noise=None,
+                 action_noise=action_noise,
+                 tensorboard_log="tensorboard/",
+                 gamma=0.99,
+                 n_cpu_tf_sess=1,
+                 random_exploration=0.0)
+
+    model.learn(total_timesteps=time_steps,
+                callback=[mean_hundred_eps_callback, succ_rate_callback, every_n_steps_callback, eval_callback],
+                log_interval=10)
+
+    model.save("DDPG_model")
+
+    # results_plotter.plot_results([log_dir], time_steps, results_plotter.X_TIMESTEPS, "DDPG")
+    # plt.show()
 
 
 if __name__ == '__main__':
